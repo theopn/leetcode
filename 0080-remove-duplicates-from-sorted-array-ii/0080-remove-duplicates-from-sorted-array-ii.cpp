8bd405c69ace4ec5fc5ce75e4bb2c3320f3d3777
Theo P <63016528+theopn@users.noreply.github.com>
2023-08-30 04:44:56 -0400

    Time: 0 ms (100.00%), Space: 11 MB (26.77%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -4,13 +4,13 @@ public:
         if (nums.size() <= 2) {
             return nums.size();
         }
-        int j = 2;
+        int pos = 2;
         for (int i = 2; i < nums.size(); i++) {
-            if (nums[i] != nums[j - 2]) {
-                nums[j] = nums[i];
-                j++;
+            if (nums[i] != nums[pos - 2]) {
+                nums[pos] = nums[i];
+                pos++;
             }
         }
-        return j;
+        return pos;
     }
 };
