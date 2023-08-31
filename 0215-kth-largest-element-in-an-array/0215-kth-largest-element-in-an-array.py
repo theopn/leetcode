c0493130be8be5ff9d0f0e4f45f11d47e9bbe72a
Theo P <63016528+theopn@users.noreply.github.com>
2023-08-31 09:13:40 -0400

    Time: 513 ms (40.33%), Space: 30.3 MB (18.56%) - LeetHub

---
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        
+        def quickselect(nums, k):
+            # Randomly choose a pivot value
+            pivot = random.choice(nums) # pivot value
+            
+            # Roughtly "sort" the array by determining its position relative to the pivot
+            left = [] # elements smaller than the pivot
+            mid = [] # elements equal to the pivot
+            right = [] # elements greater than the pivot
+            for num in nums:
+                if num == pivot:
+                    mid.append(num)
+                elif num < pivot:
+                    left.append(num)
+                else:
+                    right.append(num)
+             
+            if len(left) <= k < len(left) + len(mid):
+                # Base case: (n-k)th element is in the mid array, i.e., it's the pivot
+                # e.g., 4th largest
+                return mid[0] # or return pivot
+            elif k < len(left):
+                # Case: kth largest is in the left array (i.e., smaller than the pivot)
+                return quickselect(left, k)
+            else:
+                # Case: kth largest is in the right array (i.e., greater than the pivot)
+                return quickselect(right, k - len(left) - len(mid))
+            
+        # Instead of finding kth largest element, find (n - k)th smallest element
+        return quickselect(nums, n - k)
+                    
