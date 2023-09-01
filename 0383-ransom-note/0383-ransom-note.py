14a26efb1b8936886240ed39b4f69d8fb91c1f80
Theo P <63016528+theopn@users.noreply.github.com>
2023-09-01 04:06:02 -0400

    Time: 68 ms (55.66%), Space: 16.6 MB (62.89%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,18 +1,13 @@
-class Solution {
-public:
-    bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char, int> hashbrown;
-        for (auto m : magazine) {
-            hashbrown[m]++;
-        }
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        hashbrown = {}
+        for m in magazine:
+            hashbrown[m] = hashbrown.get(m, 0) + 1 # get() provides a default val
+         
+        for r in ransomNote:
+            if hashbrown.get(r, 0) == 0:
+                return False
+            hashbrown[r] -= 1
+            
+        return True
         
-        for (auto r : ransomNote) {
-            if (hashbrown[r] == 0) {
-                return false;
-            }
-            hashbrown[r]--;
-        }
-        
-        return true;
-    }
-};
