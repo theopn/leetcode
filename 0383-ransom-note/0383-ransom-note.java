f6ff62deed0e2e8158e6296e9b2b52fa38026d7d
Theo P <63016528+theopn@users.noreply.github.com>
2023-09-01 04:18:20 -0400

    Time: 15 ms (43.31%), Space: 44 MB (58.51%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,20 +1,11 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         HashMap<Character, Integer> hashbrown = new HashMap<>();
-        
-        //for (int i = 0; i < magazine.length(); i++) {
-        //    hashbrown.put(magazine.charAt(i), hashbrown.getOrDefault(magazine.charAt(i), 0) + 1);
-        //}
+
         for (char m : magazine.toCharArray()) {
             hashbrown.put(m, hashbrown.getOrDefault(m, 0) + 1);
         }
         
-        //for (int i = 0; i < ransomNote.length(); i++) {
-        //    if (hashbrown.getOrDefault(ransomNote.charAt(i), 0) == 0) {
-        //        return false;
-        //    }
-        //    hashbrown.put(ransomNote.charAt(i), hashbrown.getOrDefault(ransomNote.charAt(i), 0) - 1);
-        //}
         for (char r : ransomNote.toCharArray()) {
             if (hashbrown.getOrDefault(r, 0) == 0) {
                 return false;
