b4bcae5713615afbe3c48da4ed6c45d0e5c83a70
Theo P <63016528+theopn@users.noreply.github.com>
2023-09-01 04:18:04 -0400

    Time: 9 ms (64.21%), Space: 43.6 MB (87.19%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -2,25 +2,25 @@ class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         HashMap<Character, Integer> hashbrown = new HashMap<>();
         
-        for (int i = 0; i < magazine.length(); i++) {
-            hashbrown.put(magazine.charAt(i), hashbrown.getOrDefault(magazine.charAt(i), 0) + 1);
-        }
-        //for (char m : magazine.toCharArray()) {
-        //    hashbrown.put(m, hashbrown.getOrDefault(m, 0) + 1);
+        //for (int i = 0; i < magazine.length(); i++) {
+        //    hashbrown.put(magazine.charAt(i), hashbrown.getOrDefault(magazine.charAt(i), 0) + 1);
         //}
-        
-        for (int i = 0; i < ransomNote.length(); i++) {
-            if (hashbrown.getOrDefault(ransomNote.charAt(i), 0) == 0) {
-                return false;
-            }
-            hashbrown.put(ransomNote.charAt(i), hashbrown.getOrDefault(ransomNote.charAt(i), 0) - 1);
+        for (char m : magazine.toCharArray()) {
+            hashbrown.put(m, hashbrown.getOrDefault(m, 0) + 1);
         }
-        //for (char r : ransomNote.toCharArray()) {
-        //    if (hashbrown.getOrDefault(r, 0) == 0) {
+        
+        //for (int i = 0; i < ransomNote.length(); i++) {
+        //    if (hashbrown.getOrDefault(ransomNote.charAt(i), 0) == 0) {
         //        return false;
         //    }
-        //    hashbrown.put(r, hashbrown.getOrDefault(r, 0) - 1);
+        //    hashbrown.put(ransomNote.charAt(i), hashbrown.getOrDefault(ransomNote.charAt(i), 0) - 1);
         //}
+        for (char r : ransomNote.toCharArray()) {
+            if (hashbrown.getOrDefault(r, 0) == 0) {
+                return false;
+            }
+            hashbrown.put(r, hashbrown.getOrDefault(r, 0) - 1);
+        }
         
         return true;
     }
