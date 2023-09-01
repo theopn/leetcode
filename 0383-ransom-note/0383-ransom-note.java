fdf9443bec014631390d4196ef307dee288199e3
Theo P <63016528+theopn@users.noreply.github.com>
2023-09-01 04:14:57 -0400

    Time: 14 ms (49.15%), Space: 43.9 MB (58.51%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,13 +1,27 @@
-class Solution:
-    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        hashbrown = {}
-        for m in magazine:
-            hashbrown[m] = hashbrown.get(m, 0) + 1 # get() provides a default val
-         
-        for r in ransomNote:
-            if hashbrown.get(r, 0) == 0:
-                return False
-            hashbrown[r] -= 1
-            
-        return True
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap<Character, Integer> hashbrown = new HashMap<>();
         
+        //for (int i = 0; i < magazine.length(); i++) {
+        //    hashbrown[ransomNote[i]]++;
+        //}
+        for (char m : magazine.toCharArray()) {
+            hashbrown.put(m, hashbrown.getOrDefault(m, 0) + 1);
+        }
+        
+        //for (int i = 0; i < ransomNote.length(); i++) {
+        //    if (hashbrown[ransomeNote[i]] == 0) {
+        //        return false;
+        //    }
+        //    hashbrown[ransomNote[i]]--;
+        //}
+        for (char r : ransomNote.toCharArray()) {
+            if (hashbrown.getOrDefault(r, 0) == 0) {
+                return false;
+            }
+            hashbrown.put(r, hashbrown.getOrDefault(r, 0) - 1);
+        }
+        
+        return true;
+    }
+}
