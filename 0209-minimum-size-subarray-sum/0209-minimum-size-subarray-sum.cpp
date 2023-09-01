a4787c9019b028b3f09406a3dd805dae24a6652b
Theo P <63016528+theopn@users.noreply.github.com>
2023-09-01 10:59:34 -0400

    Time: 33 ms (83.06%), Space: 28.2 MB (26.73%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int sum = 0;
-        int head_pos = 0;
-        int min_len = nums.size() + 69;
+        int sum = 0; // Sum from the beginning to the current window tail
+        int head_pos = 0; // Beginning of the "window"
+        int min_len = nums.size() + 69; // If this val never changed, return 0
         
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++) { // Let's slide the window
             sum += nums[i];
+            
+            // Move the head (shrink the window) while it meets the condition
             while (sum >= target) {
+                // Update the min_len if the current window is smaller
                 if (min_len > i + 1 - head_pos) {
                     min_len = i + 1 - head_pos;
                 }
+                // Remove the head from the window
                 sum -= nums[head_pos];
                 head_pos++;
             }
