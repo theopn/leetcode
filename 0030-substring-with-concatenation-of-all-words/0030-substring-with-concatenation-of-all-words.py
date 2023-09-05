727a0003db78b775b6ffd7a4e2f59f51acbb4eaa
Theo P <63016528+theopn@users.noreply.github.com>
2023-09-05 03:15:38 -0400

    Time: 3067 ms (51.42%), Space: 17 MB (59.44%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,21 +1,19 @@
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        word_hash = {}
-        for word in words:
-            word_hash[word] = word_hash.get(word, 0) + 1
+        words_counter = Counter(words)
         word_len = len(words[0])
         total_len = word_len * len(words)
         ans = []
         
-        for i in range(0, len(s)):
-            curr_hash = {}
+        for i in range(0, len(s) - total_len + 1): # We could loop through len(s), but that would be a waste
+            curr_words = []
             window = s[i:i + total_len]
         
             for j in range(0, len(window), word_len):
                 curr_word = window[j:j + word_len]
-                curr_hash[curr_word] = curr_hash.get(curr_word, 0) + 1
+                curr_words.append(curr_word)
         
-            if curr_hash == word_hash:
+            if words_counter == Counter(curr_words):
                 ans.append(i)
         
         return ans
