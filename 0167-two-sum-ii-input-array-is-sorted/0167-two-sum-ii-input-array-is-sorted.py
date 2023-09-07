75d5883865c2cf2c7cf6c965b2cf17c88ee15549
Theo P <63016528+theopn@users.noreply.github.com>
2023-09-07 08:04:13 -0400

    Time: 14 ms (35.13%), Space: 15.6 MB (32.33%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,16 +1,20 @@
-class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        front, end = 0, len(numbers) - 1
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int front = 0, end = numbers.size() - 1;
         
-        while front < end:
-            sum = numbers[front] + numbers[end]
+        while (front < end) {
+            int sum = numbers[front] + numbers[end];
             
-            if sum == target:
-                return [front + 1, end + 1]
-            elif sum > target:
-                end -= 1
-            else:
-                front += 1
-                
-        return [-1, -1]
+            if (sum == target) {
+                return { front + 1, end + 1 };
+            } else if (sum > target) {
+                end--;
+            } else {
+                front++;
+            }
+        }
         
+        return { -1, -1 };
+    }
+};
