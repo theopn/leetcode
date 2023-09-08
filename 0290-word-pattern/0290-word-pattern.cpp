893e4ffecd865e56819f369746cae1e366625ae7
Theo P <63016528+theopn@users.noreply.github.com>
2023-09-08 10:37:04 -0400

    Time: 0 ms (100.00%), Space: 6.4 MB (30.28%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char, string> patt_to_w;
+        unordered_map<string, char> w_to_patt;
+
+        vector<string> words;
+        stringstream ss(s);
+        string word;
+        while (ss >> word) {
+            words.push_back(word);
+        }
+
+        if (pattern.size() != words.size()) {
+            return false;
+        }
+        
+        for (int i = 0; i < pattern.size(); i++) {
+            if (patt_to_w.find(pattern[i]) == patt_to_w.end()) {
+                patt_to_w[pattern[i]] = words[i];
+            } else if (patt_to_w[pattern[i]] != words[i]) {
+                return false;
+            }
+
+            if (w_to_patt.find(words[i]) == w_to_patt.end()) {
+                w_to_patt[words[i]] = pattern[i];
+            } else if (w_to_patt[words[i]] != pattern[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
