d4468a93e331bdea5a8ca90c8d5b8a7c1edbe5ae
Theo P <63016528+theopn@users.noreply.github.com>
2023-09-08 04:15:00 -0400

    Time: 48 ms (60.58%), Space: 16.7 MB (43.14%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,17 +1,18 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        hashbrown = {}
+        if len(s) != len(t): return False
+
+        s_to_t = {}
+        t_to_s = {}
         for i in range(len(s)):
-            if not s[i] in hashbrown:
-                hashbrown[s[i]] = t[i]
-            elif hashbrown[s[i]] != t[i]:
+            if not s[i] in s_to_t:
+                s_to_t[s[i]] = t[i]
+            elif s_to_t[s[i]] != t[i]:
                 return False
             
-        hashbrown = {}
-        for i in range(len(t)):
-            if not t[i] in hashbrown:
-                hashbrown[t[i]] = s[i]
-            elif hashbrown[t[i]] != s[i]:
+            if not t[i] in t_to_s:
+                t_to_s[t[i]] = s[i]
+            elif t_to_s[t[i]] != s[i]:
                 return False
             
         return True
