df19b263bd9986a97a7ef321ddbd1c44aaf3030a
Theo P <63016528+theopn@users.noreply.github.com>
2023-09-11 07:03:30 -0400

    Time: 12 ms (73.35%), Space: 16.5 MB (68.09%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        vector<int> dp{ nums[0] };
         for (int i = 1; i < nums.size(); i++) {
-            dp.push_back(max(i + nums[i], dp[i - 1]));
+            nums[i] = max(i + nums[i], nums[i - 1]);
         }
 
         int jumps = 0;
         int idx = 0;
         while (idx < nums.size() - 1) {
             jumps++;
-            idx = dp[idx];
+            idx = nums[idx];
         }
 
         return jumps;
