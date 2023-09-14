34901bb8d91fc392e1ea5a386262edb788ca4baa
Theo P <63016528+theopn@users.noreply.github.com>
2023-09-14 02:35:18 -0400

    Time: 7 ms (91.17%), Space: 11.3 MB (12.56%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> complements; // key = nums[i] val = i
+        
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if ((complements.count(complement) > 0) && (complements[complement] != i)) {
+                return { complements[complement], i };
+            }
+            complements[nums[i]] = i;
+        }
+        
+        return { -1, -1 };
+    }
+};
